8121d5137a72b7e47903067c4f672ca0be18c4fd
Nemanja Nikolć <58153223+nexencode@users.noreply.github.com>
2020-06-05 15:01:59 +0200

    Add project files.

---
+++ WSP.Bl/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WSP.Bl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
